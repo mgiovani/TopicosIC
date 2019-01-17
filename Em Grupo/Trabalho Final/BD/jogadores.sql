db15a3e4f61f5a7d4ea7886c7e7ace952d35a673
Giovani Moutinho <giovanim@ymail.com>
2019-01-16 22:15:08 -0200

    Ajustes de funções de inserção do BD

--- Em Grupo/Trabalho Final/BD/jogadores.sql
+++
@@ -1,2 +0,0 @@
-
-INSERT INTO jogador('summonerId', 'nick', 'elo', 'pdl', 'vitorias', 'derrotas', 'winrate', 'posicao1', 'posicao2') VALUES(6804307,'TheDarkDescent','Platinum 4',0,50,39,56,'?','?');
